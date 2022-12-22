249d62a84e32685c5a29bb64d41a1b4e9c48618d
Rustie Lin <rustie117@gmail.com>
2022-12-21 17:03:32 -0800

    remove deprecated genesis scripts

--- generate_keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aptos genesis generate-keys --output-dir keys/aptos-google-na-1
-aptos genesis generate-keys --output-dir keys/aptos-google-na-2
-aptos genesis generate-keys --output-dir keys/aptos-google-na-3
-aptos genesis generate-keys --output-dir keys/aptos-google-europe-1
-aptos genesis generate-keys --output-dir keys/aptos-google-europe-2
-aptos genesis generate-keys --output-dir keys/aptos-google-asia-2
-aptos genesis generate-keys --output-dir keys/aptos-google-asia-1
